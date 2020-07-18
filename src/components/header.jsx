bb4d71d3a333628c3d0100fc16da383ff91bdc06
Cdric_Art <59844347+Cdric-art@users.noreply.github.com>
2020-07-18 15:18:07 +0200

    Start project

---
+++ src/components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Header () {
+
+    return ( 
+        <div className="Header container-fluid">
+            <div className="Icon-React"></div>
+            <h1>Je m'appelle Cédric,</h1>
+            <h2>Je suis Développeur Intégrateur Web.</h2>
+            <a className="Mail" href="mailto:cdric.art@gmail.com">cdric.art@gmail.com<span> || Paris VI</span></a>
+        </div>
+    )
+}
+
+export default Header;
