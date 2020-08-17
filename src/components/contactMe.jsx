429ebcc8c2bb6007fd5d69a5b91877900a8cabcd
Evrard Cedric <cdric.art@gmail.com>
2020-08-17 12:07:41 +0200

    Replace close.png Style change for Modal

--- src/components/contactMe.jsx
+++ src/components/contactMe.jsx
@@ -11,9 +11,9 @@ class ContactMe extends React.Component {
                 </div>
                 <form className="form-modal" method="POST">
                     <input type="text" name="prenom" placeholder="Votre prénom :"/>
-                    <input type="email" name="email" placeholder="Votre E-Mail :"/>
+                    <input type="email" name="email" placeholder="Votre email :"/>
                     <textarea name="message" cols="30" rows="5" placeholder="Votre message :"/>
-                    <p>Vos données personnelles (email et nom d'utilisateur) ne sont utilisées que pour me permettre de vous adresser une réponse et ne sont pas partagé avec des tiers.</p>
+                    <p>Vos données personnelles (prénom et email) ne sont utilisées que pour me permettre de vous adresser une réponse et ne sont pas partagé avec des tiers.</p>
                     <button onClick={ this.props.close } className="btn-modal">Envoyer</button>
                 </form>
             </div>
