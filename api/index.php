fb2d7136fc8a5393de26b0abf1082afd3a4397e5
Evrard Cedric <cdric.art@gmail.com>
2020-09-07 15:57:38 +0200

    Ref : cv-icon Ref : icon-react.png Ref : meta og

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 
 $pdo = new PDO(
     'mysql:host=cdricaoreactjs.mysql.db;dbname=cdricaoreactjs',
