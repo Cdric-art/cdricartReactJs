f28ab74354a9bdfd8014ed5cea6446df37ab3436
Cdric-art <cdric.art@gmail.com>
2021-03-01 17:15:46 +0100

    Fix: Delete gif Feat: Base structure new component project.jsx

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -2,9 +2,34 @@ import React from 'react';
 
 const Project = () => {
     return (
-        <div className="projects">
-            bla
-        </div>
+        <section className="projects">
+            <div className="wrapper-projects">
+                <div className="project-item">
+                    <img src="" alt="Project Pineapple"/>
+                    <h3>PINEAPPLE</h3>
+                    <span/>
+                    <p>FullStack JS</p>
+                </div>
+                <div className="project-item">
+                    <img src="" alt="Project Recipes App"/>
+                    <h3>RECIPES APP</h3>
+                    <span/>
+                    <p>FullStack JS</p>
+                </div>
+                <div className="project-item">
+                    <img src="" alt="Project Wp-React"/>
+                    <h3>ARTISTE</h3>
+                    <span/>
+                    <p>Wordpress - React Js</p>
+                </div>
+                <div className="project-item">
+                    <img src="" alt="Project Weather App"/>
+                    <h3>WEATHER APP</h3>
+                    <span/>
+                    <p>React Js</p>
+                </div>
+            </div>
+        </section>
     );
 };
 
