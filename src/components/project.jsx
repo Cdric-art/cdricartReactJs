91b26e923f2bcbdd30bf49a4fdfe707c2000adfd
Cdric-art <cdric.art@gmail.com>
2021-03-02 15:41:16 +0100

    Feat: Structure details projects

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,9 +1,13 @@
 import React, {useState} from 'react';
 
 import Pineapple from '../assets/images/pineapple.png';
-import Recipes from '../assets/images/recipe.png';
+import Pineapple1 from '../assets/images/pineapple1.png';
+import Pineapple2 from '../assets/images/pineapple2.png';
+import Pineapple3 from '../assets/images/pineapple3.png';
+import Whire from '../assets/images/whire.png';
 import Artiste from '../assets/images/artiste.png';
 import Weather from '../assets/images/weather.png';
+import Close from '../assets/icons/close.png';
 
 const Project = () => {
 
@@ -25,45 +29,84 @@ const Project = () => {
                 <div className="project-item" data-index={0} onClick={handleDetail}>
                     <img src={Pineapple} alt="Project Pineapple"/>
                     <h3>PINEAPPLE</h3>
-                    <span/>
+                    <span className="hr-blue"/>
                     <p>FullStack JS</p>
                 </div>
                 <div className="project-item" data-index={1} onClick={handleDetail}>
-                    <img src={Recipes} alt="Project Recipes App"/>
-                    <h3>RECIPES APP</h3>
-                    <span/>
-                    <p>FullStack JS</p>
+                    <img src={Whire} alt="Project Whire"/>
+                    <h3>Whire</h3>
+                    <span className="hr-blue"/>
+                    <p>React JS</p>
                 </div>
                 <div className="project-item" data-index={2} onClick={handleDetail}>
                     <img src={Artiste} alt="Project Wp-React"/>
                     <h3>ARTISTE</h3>
-                    <span/>
-                    <p>Wordpress - React Js</p>
+                    <span className="hr-blue"/>
+                    <p>Wordpress - React JS</p>
                 </div>
                 <div className="project-item" data-index={3} onClick={handleDetail}>
                     <img src={Weather} alt="Project Weather App"/>
                     <h3>WEATHER APP</h3>
-                    <span/>
-                    <p>React Js</p>
+                    <span className="hr-blue"/>
+                    <p>React JS</p>
                 </div>
             </div>
             <div onClick={() => setShowDetails(false)} className={showDetails ? 'wrapper-details active' : 'wrapper-details'}>
                 {index === 0 ? (
-                    <div className="detail-body">
-                        Pineapple
-                    </div>
+                    <>
+                        <div className="detail-header">
+                            <img src={Close} alt="Close"/>
+                            <h3>Pineapple</h3>
+                        </div>
+                        <div className="detail-body">
+                            <div className="images">
+                                <img src={Pineapple1} alt="Pineapple 1"/>
+                                <img src={Pineapple2} alt="Pineapple 2"/>
+                                <img src={Pineapple3} alt="Pineapple 3"/>
+                            </div>
+                            <div className="text">
+                                <div className="description">
+                                    <h3>Description</h3>
+                                    <span className="hr-blue"/>
+                                    <p>Bla</p>
+                                </div>
+                                <div className="infosup">
+                                    <h3>Infos</h3>
+                                    <span className="hr-blue"/>
+                                </div>
+                            </div>
+                        </div>
+                    </>
                 ) : index === 1 ? (
-                    <div className="detail-body">
-                        Recipe
-                    </div>
+                    <>
+                        <div className="detail-header">
+                            <img src={Close} alt="Close"/>
+                            <h3>Whire</h3>
+                        </div>
+                        <div className="detail-body">
+
+                        </div>
+                    </>
                 ) : index === 2 ? (
-                    <div className="detail-body">
-                        Artiste
-                    </div>
+                    <>
+                        <div className="detail-header">
+                            <img src={Close} alt="Close"/>
+                            <h3>Artiste</h3>
+                        </div>
+                        <div className="detail-body">
+
+                        </div>
+                    </>
                 ) : index === 3 ? (
-                    <div className="detail-body">
-                        Weather
-                    </div>
+                    <>
+                        <div className="detail-header">
+                            <img src={Close} alt="Close"/>
+                            <h3>Weather App</h3>
+                        </div>
+                        <div className="detail-body">
+
+                        </div>
+                    </>
                 ) : null}
             </div>
         </section>
